5deae62d240e068f52dd431eec89b79541494f5c
leonardoarturoalpizarperez <usr.tmp1.mx@gmail.com>
2026-02-09 18:02:02 -0600

    Demo Services Cloud v5

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO team (id, coach, team_year) VALUES (1, 'Dominique Ducharme', 2022);
+INSERT INTO player (number, name, lastname, position, is_captain, team_id) VALUES (31, 'Carey', 'Price', 'goaltender', false, 1);
+INSERT INTO player (number, name, lastname, position, is_captain, team_id) VALUES (6, 'Shea', 'Weber', 'defenseman', true, 1);
