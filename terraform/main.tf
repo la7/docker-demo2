3a74c79a62123dacfcfd7efc4bec386060a97670
leonardoarturoalpizarperez <usr.tmp1.mx@gmail.com>
2026-02-11 11:27:32 -0600

    Demo Services Cloud v22 Deploy local

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,16 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "256"
   memory                   = "512"
 
+  resource "aws_ecs_task_definition" "app" {
+    family                   = "spring-app-task"
+    container_definitions = jsonencode([
+      {
+        name  = "spring-app" # <-- Coincide con container-name en deploy.yml
+        image = "000000000000.dkr.ecr.us-east-1.amazonaws.com/springboot-app:latest"
+        # ... resto de la config
+      }
+    ])
+  }
   container_definitions = jsonencode([
     {
       name      = "spring-app" # Coincide con container-name en deploy.yml
